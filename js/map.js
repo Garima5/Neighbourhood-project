92347c238abec1249981a1480e74179a7dca858d
Garima5 <Garima5@users.noreply.github.com>
2017-02-02 08:20:29 +0530

    Update map.js

--- js/map.js
+++ js/map.js
@@ -227,5 +227,18 @@ function stopAnimation(marker) {
         marker.setAnimation(null);
     }, 3000);
 }
+function googleError()
+{
+    var cssClass;
+    var text;
+    if (typeof google === 'undefined') {
+  cssClass = 'not-loaded';
+  text += 'not loaded 😞';
+} else {
+  cssClass = 'loaded';
+  text += 'loaded 😀'
+}
+console.log(text);
 
+}
 /////////////
